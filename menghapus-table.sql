9a253626b058468db3f82ab0f7575f18411268c7
koreoxy <rayssankn@gmail.com>
2023-11-09 17:06:17 +0700

    add query menghapus table

---
+++ menghapus-table.sql
@@ -0,0 +1,14 @@
+-- Hapus Tabel DetailPesanan
+DROP TABLE DetailPesanan;
+
+-- Hapus Tabel Pesanan
+DROP TABLE Pesanan;
+
+-- Hapus Tabel Pelanggan
+DROP TABLE Pelanggan;
+
+-- Hapus Tabel Produk
+DROP TABLE Produk;
+
+-- Hapus Tabel Kategori
+DROP TABLE Kategori;
