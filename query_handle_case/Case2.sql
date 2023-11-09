9ac5b036f311cd77365da30784cc43715a632e68
koreoxy <rayssankn@gmail.com>
2023-11-09 17:45:06 +0700

    fix query case 2

--- query_handle_case/Case2.sql
+++ query_handle_case/Case2.sql
@@ -1,8 +1,8 @@
 -- Query untuk menjalankan perintah berikut ini:
 -- Melihat 3 produk yang paling sering dibeli oleh pelanggan.
-SELECT Produk.nama_produk, COUNT(*) AS jumlah
+SELECT Produk.nama_produk, COUNT(*) AS jumlah_pembelian
 FROM DetailPesanan
 JOIN Produk ON DetailPesanan.produk_id = Produk.produk_id
 GROUP BY Produk.produk_id
-ORDER BY jumlah DESC
+ORDER BY jumlah_pembelian DESC
 LIMIT 3;
